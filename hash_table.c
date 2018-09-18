cab462fea2dd7ea2521ee78a0ed8c64aa14300f4
brr3 <36078418+brr3@users.noreply.github.com>
2018-09-18 11:43:10 +0200

    assignment1_step1

--- hash_table.c
+++ hash_table.c
@@ -11,6 +11,7 @@ ioopm_hash_table_t *ioopm_hash_table_create()
   return result;
 }
 
+
 static entry_t *find_previous_entry_for_key(entry_t *address, int key)
 {
   entry_t *first_entry = address;
@@ -20,23 +21,32 @@ static entry_t *find_previous_entry_for_key(entry_t *address, int key)
     {
       if (cursor->key == key)
         {
+          // printf("Cursor key found: %p\n", cursor);
           return cursor; /// Ends the whole function!
-        } 
+        }
+      // printf("Cursor in loop: %p\n", cursor->next);
       cursor = cursor->next; /// Step forward to the next entry, and repeat loop
     }
-  return cursor;
+  //printf("Cursor key not found: %p\n", cursor);
+  //printf("first_entry: %p\n", first_entry);
+  first_entry->next = NULL;
+  first_entry->key = key;
+  return first_entry;
 }
 
 
-static entry_t *entry_create(int key, char *value, entry_t *next)
+entry_t *entry_create(int key, char *value, entry_t *next)
 {
   entry_t *new_entry = calloc(1, sizeof(entry_t));
 
   new_entry->key = key;
   new_entry->value = value;
   new_entry->next = next;
+  //printf("new_entry value: %s\n", value);
+  //printf("new_entry next: %p\n", next);
+  //printf("new_entry pointer: %p\n", new_entry);
 
-  return new_entry; 
+  return new_entry;                      
 }
 
 
@@ -45,9 +55,12 @@ void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value)
   /// Calculate the bucket for this entry
   int bucket = key % 17;
   /// Search for an existing entry for a key
+  //printf("Initial value: %s\n", value);
   entry_t *entry = find_previous_entry_for_key(&ht->buckets[bucket], key);
-  printf("%p \n", next);
+  //printf("Entry key: %d\n", entry->key);
+  //printf("Entry value: %s\n", entry->value);
   entry_t *next = entry->next;
+  //printf("Entry next: %p\n", next);
 
   /// Check if the next entry should be updated or not
   if (next != NULL && next->key == key)
@@ -57,6 +70,8 @@ void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value)
   else
     {
       entry->next = entry_create(key, value, next);
+      ht->buckets[bucket] = *entry->next;
+      //printf("Pointer at key %d: %p\n", key, ht->buckets[bucket].value);
     }
 }
 
--- main.c
+++ main.c
@@ -3,23 +3,54 @@
 #include "hash_table.h"
 
 
-void test_lookup1()
-{
-  ioopm_hash_table_t *h = ioopm_hash_table_create();
-  int key = 1;
-  char *value = "a";
-  ioopm_hash_table_insert(h, key, value);
-}
-
-
-int main(void)
+void test_insert1()
 {
   ioopm_hash_table_t *h = ioopm_hash_table_create();
   int key = 1;
   char *value = "abc";
   int bucket = key % 17;
-  printf("Initial value: %s", value);
   ioopm_hash_table_insert(h, key, value);
-  printf("Value at key %d: %s", key, h->buckets[bucket].value);
+  printf("Value at key %d: %s\n", key, h->buckets[bucket].value);
+}
+
+
+void test_insert2()
+{
+  ioopm_hash_table_t *h = ioopm_hash_table_create();
+  int key = 1;
+  char *value1 = "abc";
+  char *value2 = "cde";
+  int bucket = key % 17;
+  ioopm_hash_table_insert(h, key, value1);
+  printf("Value 1 at key %d: %s\n", key, h->buckets[bucket].value);
+  ioopm_hash_table_insert(h, key, value2);
+  printf("Value 2 at key %d: %s\n", key, h->buckets[bucket].value);
+}
+
+
+void test_insert3()
+{
+  ioopm_hash_table_t *h = ioopm_hash_table_create();
+  int key1 = 1;
+  int key2 = 2;
+  char *value1 = "abc";
+  char *value2 = "cde";
+  int bucket1 = key1 % 17;
+  int bucket2 = key2 % 17;
+  ioopm_hash_table_insert(h, key1, value1);
+  printf("Value 1 at key %d: %s\n", key1, h->buckets[bucket1].value);
+  ioopm_hash_table_insert(h, key2, value2);
+  printf("Value 2 at key %d: %s\n", key2, h->buckets[bucket2].value);
+}
+
+
+int main(void)
+{
+  //puts("Test 1 Insert");
+  //test_insert1();
+  //puts("Test 2 Insert");
+  //test_insert2();
+  puts("Test 3 Insert");
+  test_insert3();
   return 0;
 }
